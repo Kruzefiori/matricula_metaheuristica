195991a687d75733a97329551117e78c3a8e4d5e
Augusto <augusto.lr06@gmail.com>
2025-03-23 01:46:08 -0300

    feat: improve output json file name

--- src/__init__.py
+++ src/__init__.py
@@ -7,18 +7,19 @@ def main():
     parser.add_argument('--dataset_name', required=True)
     args = parser.parse_args()
 
-
+    # Extrair as tabelas do PDF
     tables = pdf.getTablesFromPDF(args)
+    # Processar as tabelas
     final_json_unformatted = pdf.processTables(tables)
 
     # Formatar o JSON
     final_json = json.dumps(json.loads(final_json_unformatted), indent=2, ensure_ascii=False)
 
     # Salvar o JSON, com o nome do dataset
-    with open(f"output/{args.dataset_name}.json", "w") as f:
+    datasetName = str(args.dataset_name).replace('.pdf', '')
+    with open(f"datasets/{datasetName}.json", "w") as f:
         f.write(final_json)
-        print(f"JSON salvo em output/{args.dataset_name}.json")
-    
+        print(f"JSON salvo em datasets/{datasetName}.json")
 
 if __name__ == "__main__":
     main()
